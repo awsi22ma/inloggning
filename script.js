fb168bb2101ae838e69b738f9b7e78a3a1f52cdd
silva <silva.88s@live.com>
2022-10-23 09:52:32 +0200

    ny ändring

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const name = document.getElementById("username");
-const psw = document.getElementById("password");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 const login = document.getElementById("login");
 const logut = document.getElementById("logut");
 
@@ -7,7 +7,7 @@ let userChecker = localStorage.getItem("username");
 let passChecker = localStorage.getItem("password");
 
 
-if (userChecker == 'Sara' && passChecker == 'qwe123' );  { 
+if (userChecker == 'Sara' && passChecker == 'qwe123' ) { 
 
 
 
@@ -20,7 +20,7 @@ login.addEventListener("click", function () {
 
   if (username.value == "Sara" && password.value == "qwe123") {
 
-      localStorage.setItem("usernamn", "Sara");
+      localStorage.setItem("username", "Sara");
       localStorage.setItem("password", "qwe123");
       document.getElementById("demo").style.display = "none";
       
@@ -40,8 +40,7 @@ login.addEventListener("click", function () {
 
 
 
-    localStorage.removeItem("usernamn", "Sara");
-    localStorage.removeItem("password", "qwe123");
+   localStorage.clear();
     document.getElementById("demo").style.display = "block";
 
     document.getElementById("logut").style.display = "none";
